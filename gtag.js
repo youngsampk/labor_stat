f44bfac632e27c096d71504e9fc6539e2365cd1d
youngsampk <youngsampk@gmail.com>
2024-11-23 03:21:55 +0900

    for google analytics

---
+++ gtag.js
@@ -0,0 +1,8 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script><script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-60S2W83K5X');
+</script>
